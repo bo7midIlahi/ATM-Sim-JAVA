9a3feed09a1ff81ef142632c47a807578f3eb054
bohmid <elbo7ilahi@gmail.com>
2026-04-19 13:04:27 +0100

    add: window shows up

---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class MainWindow {
+    private JFrame window;
+    public MainWindow(){
+        window = new JFrame(); //create window
+        window.setTitle("ATM"); //set title for atm
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //set cross button to close window when clicked
+        window.setSize(600,800); //set size
+        window.setLocationRelativeTo(null); //center window
+    }
+
+    public  void show(){
+        window.setVisible(true);
+    }
+}
