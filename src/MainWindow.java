bc44547f7b99e09d3d86c49c50c45f7c0dea7f47
bohmid <elbo7ilahi@gmail.com>
2026-04-19 14:37:35 +0100

    add: create layout (BorderLayout)

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,5 @@
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainWindow {
@@ -7,6 +9,15 @@ public class MainWindow {
         window.setTitle("ATM"); //set title for atm
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //set cross button to close window when clicked
         window.setSize(800,600); //set size
+        window.setLayout(new BorderLayout(5,5));
+
+        window.add(new JButton("NORTH"), BorderLayout.NORTH);
+        window.add(new JButton("WEST"), BorderLayout.WEST);
+        window.add(new JButton("EAST"), BorderLayout.EAST);
+        window.add(new JButton("SOUTH"), BorderLayout.SOUTH);
+        window.add(new JButton("CENTER1"), BorderLayout.CENTER);
+        window.add(new JButton("CENTER2"), BorderLayout.CENTER);
+
         window.setLocationRelativeTo(null); //center window
     }
 
