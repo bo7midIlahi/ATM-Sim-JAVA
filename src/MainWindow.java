47e84ab730725c99e08757296e2942374c585d98
bohmid <elbo7ilahi@gmail.com>
2026-04-19 15:04:02 +0100

    added btns in the center

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,12 +16,44 @@ public class MainWindow {
         window.setSize(800,600); //set size
         window.setLayout(new BorderLayout(5,5));
 
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(); //Panel is a container for other Swing components
         panel.setLayout(new FlowLayout(FlowLayout.CENTER,10,5));
+        //panel.setBackground(Color.black);
+
+        //creating buttons
+        Button btnDepost = new Button("DEPOSIT");
+        Button btnCahsWithdrawl = new Button("CASH WITHDRAWL");
+        Button btnPinChange = new Button("PIN CHANGE");
+        Button btnBalanceEquity = new Button("BALANCE EQUITY");
+        Button btnTransfer = new Button("TRANSFER");
+        Button btnMiniStatement = new Button("MINI STATEMENT");
+        
+        //setting size for btns
+        btnDepost.setPreferredSize(new Dimension(350, 100));
+        btnCahsWithdrawl.setPreferredSize(new Dimension(350, 100));
+        btnPinChange.setPreferredSize(new Dimension(350, 100));
+        btnBalanceEquity.setPreferredSize(new Dimension(350, 100));
+        btnTransfer.setPreferredSize(new Dimension(350, 100));
+        btnMiniStatement.setPreferredSize(new Dimension(350, 100));
+
+        //setting fonst size
+        btnDepost.setFont(new Font("Serif", Font.BOLD, 30));
+        btnCahsWithdrawl.setFont(new Font("Serif", Font.BOLD, 30));
+        btnPinChange.setFont(new Font("Serif", Font.BOLD, 30));
+        btnBalanceEquity.setFont(new Font("Serif", Font.BOLD, 30));
+        btnTransfer.setFont(new Font("Serif", Font.BOLD, 30));
+        btnMiniStatement.setFont(new Font("Serif", Font.BOLD, 30));
+
+        //adding buttons to panel
+        panel.add(btnDepost);
+        panel.add(btnCahsWithdrawl);
+        panel.add(btnPinChange);
+        panel.add(btnBalanceEquity);
+        panel.add(btnTransfer);
+        panel.add(btnMiniStatement);
+
 
         window.add(new JButton("NORTH"), BorderLayout.NORTH);
-        window.add(new JButton("WEST"), BorderLayout.WEST);
-        window.add(new JButton("EAST"), BorderLayout.EAST);
         window.add(new JButton("SOUTH"), BorderLayout.SOUTH);
         window.add(panel, BorderLayout.CENTER);
 
